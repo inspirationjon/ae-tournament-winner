4ff947675da7386175271782522757c0bdfef41a
inspirationjon <ilkhomjon.interview@gmail.com>
2024-02-26 12:24:46 -0600

    First Commit

---
+++ sol1.ts
@@ -0,0 +1,10 @@
+export function tournamentWinner(competitions: string[][], results: number[]) {
+
+    const winnerNames = new Map()
+
+    for (let idx = 0; idx < competitions.length; idx++) {
+
+    }
+
+    return '';
+}
